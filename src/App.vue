0dfd3640174ae9b433e34240ad8924ad2fe6b82c
asantosmanu <manuelasantos452006@gmail.com>
2023-05-08 12:42:01 -0300

    entender

--- src/App.vue
+++ src/App.vue
@@ -5,128 +5,71 @@ const produtos = [
         id: 1,
         nome: 'Camiseta',
         preco: 49.90
+        quantidade: 1
     },
     {
         id: 2,
         nome: 'Calça',
         preco: 99.90
+        quantidade: 1
     },
     {
         id: 3,
         nome: 'Meia',
         preco: 9.90
+        quantidade: 1
     },
     {
         id: 4,
         nome: 'Sapato',
         preco: 199.90
+        quantidade: 1
     },
     {
         id: 5,
         nome: 'Boné',
         preco: 29.90
+        quantidade: 1
     },
     {
         id: 6,
         nome: 'Óculos',
         preco: 99.90
+        quantidade: 1
     },
     {
         id: 7,
         nome: 'Relógio',
         preco: 299.90
+        quantidade: 1
     },
     {
         id: 8,
         nome: 'Bermuda',
         preco: 79.90
+        quantidade: 1
     },
     {
         id: 9,
         nome: 'Cueca',
         preco: 19.90
+        quantidade: 1
     },
     {
         id: 10,
         nome: 'Meia',
         preco: 9.90
+        quantidade: 1
     }
 ]
-const carrinho = [
-    items: [
-    {
-      id: 1,
-      nome: 'Camiseta',
-      preco: 49.90,
-      quantidade: 1,
-      valorTotal: 49.90
-    },
-    {
-      id: 2,
-      nome: 'Calça',
-      preco: 99.90,
-      quantidade: 2,
-      valorTotal: 199.80
-    },
-    {
-      id: 3,
-      nome: 'Meia',
-      preco: 9.90,
-      quantidade: 4,
-      valorTotal: 39.60
-    }
-    {
-      id: 4,
-      nome: 'Sapato',
-      preco: 199.90
-      quantidade: 4,
-      valorTotal: 39.60
-    },
-    {
-        id: 5,
-        nome: 'Boné',
-        preco: 29.90
-        quantidade: 4,
-        valorTotal: 39.60
-    },
-    {
-        id: 6,
-        nome: 'Óculos',
-        preco: 99.90
-        quantidade: 4,
-        valorTotal: 39.60
-    },
-    {
-        id: 7,
-        nome: 'Relógio',
-        preco: 299.90
-        quantidade: 4,
-        valorTotal: 39.60
-    },
-    {
-        id: 8,
-        nome: 'Bermuda',
-        preco: 79.90
-        quantidade: 4,
-        valorTotal: 39.60
-    },
-    {
-        id: 9,
-        nome: 'Cueca',
-        preco: 19.90
-        quantidade: 4,
-        valorTotal: 39.60
-    },
-    {
-        id: 10,
-        nome: 'Meia',
-        preco: 9.90
-        quantidade: 4,
-        valorTotal: 39.60
-    }
-    ]
-    total: 288.30
-]
+const novoProduto = ref('')
+function adicionar() {
+  produtos.value.push(novoProduto.value)
+  novoProduto.value = ''
+}
+function remover(index) {
+  produtos.value.splice(index, 1)
+}
 </script>
 
 <template>
