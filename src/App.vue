7bd039aaa241e0841f4760bfde96180050ccaf60
RuiDevin <rafaelalvaro00@gmail.com>
2023-05-12 16:56:35 -0300

    botao--

--- src/App.vue
+++ src/App.vue
@@ -4,79 +4,81 @@ const produtos = [
     {
         id: 1,
         nome: 'Camiseta',
-        preco: 49.90
+        preco: 49.90,
         quantidade: 1
     },
     {
         id: 2,
         nome: 'Calça',
-        preco: 99.90
+        preco: 99.90,
         quantidade: 1
     },
     {
         id: 3,
         nome: 'Meia',
-        preco: 9.90
+        preco: 9.90,
         quantidade: 1
     },
     {
         id: 4,
         nome: 'Sapato',
-        preco: 199.90
+        preco: 199.90,
         quantidade: 1
     },
     {
         id: 5,
         nome: 'Boné',
-        preco: 29.90
+        preco: 29.90,
         quantidade: 1
     },
     {
         id: 6,
         nome: 'Óculos',
-        preco: 99.90
+        preco: 99.90,
         quantidade: 1
     },
     {
         id: 7,
         nome: 'Relógio',
-        preco: 299.90
+        preco: 299.90,
         quantidade: 1
     },
     {
         id: 8,
         nome: 'Bermuda',
-        preco: 79.90
+        preco: 79.90,
         quantidade: 1
     },
     {
         id: 9,
         nome: 'Cueca',
-        preco: 19.90
+        preco: 19.90,
         quantidade: 1
     },
     {
         id: 10,
         nome: 'Meia',
-        preco: 9.90
+        preco: 9.90,
         quantidade: 1
-    }
+    },
 ]
-const novoProduto = ref('')
-function adicionar() {
-  produtos.value.push(novoProduto.value)
-  novoProduto.value = ''
+function incrementar() {
+  quantidade.value++
 }
-function remover(index) {
-  produtos.value.splice(index, 1)
+function decrementar() {
+    quantidade.value--
 }
+
 </script>
 
 <template>
-  <ul>
-    <li v-for="(produto, index) in produtos" :key="index"> {{ produto }} </li>
-  </ul>
-  <p v-for="(produto, index) in produtos" :key="index">{{ produto }}</p>
+    <div v-for="produto in produtos" :key="produto.id">
+        <p>{{ produto.name }}</p>
+        <p>{{ produto.preco }}</p>
+        <p>{{ produto.quantidade }}</p>
+        <button @click="incrementar" {{ quantidade }}>+</button>
+    </div>
+
 </template>
 
 <style scoped>
